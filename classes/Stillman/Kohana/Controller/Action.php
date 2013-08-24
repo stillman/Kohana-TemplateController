ba7e5caf81e5b85cfb13a645ada6e6caee4cff73
Stillman <dmitry.stillman@gmail.com>
2013-08-24 22:34:46 +0300

    Require Kohana_Controller in Action constructor instead of Controller

--- classes/Stillman/Kohana/Controller/Action.php
+++ classes/Stillman/Kohana/Controller/Action.php
@@ -10,7 +10,7 @@ abstract class Action
 
 	protected $response;
 
-	public function __construct(\Controller $controller)
+	public function __construct(\Kohana_Controller $controller)
 	{
 		$this->controller = $controller;
 		$this->request = $controller->request;
